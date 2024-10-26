92727c7e5e87d2e8f1ddca99ec86af744e7266e5
tanishqcic <tanishqcic@gmail.com>
2024-10-26 17:18:49 +0530

    moved email password to env file

--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
@@ -139,7 +142,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True  # Use TLS
-EMAIL_HOST_USER = 'whoppah.marketplace@gmail.com'  # Your Gmail address
-EMAIL_HOST_PASSWORD = 'cvpf njhc yuij tups'   # The App Password you generated
-DEFAULT_FROM_EMAIL = 'whoppah.marketplace@gmail.com'  # Your Gmail address
-
+EMAIL_HOST_USER = 'whoppah.marketplace@gmail.com' 
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD') 
+DEFAULT_FROM_EMAIL = 'whoppah.marketplace@gmail.com' 
