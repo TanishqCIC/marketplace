32b6a5af069fcb2d31caccddbf0c86d5fc81d704
tanishqcic <tanishqcic@gmail.com>
2024-10-27 13:43:39 +0530

    django static files - change in dockerfile

--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -70,6 +70,7 @@ CORS_ALLOWED_ORIGINS = [
     'https://marketplace-production-a60e.up.railway.app',
 ]
 
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
@@ -158,7 +159,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = ('myproject/staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
